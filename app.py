7b5587bd11d4dce7a3b66b931538a35b34967980
Ahsan Rahul <rahul.ahsan10@gmail.com>
2026-07-09 20:27:16 +0000

    added a lot more css styling

--- app.py
+++ app.py
@@ -3,9 +3,9 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_behind_proxy import FlaskBehindProxy
 from dotenv import load_dotenv
 import os
-from google import genai
-from google.genai import types
-from pydantic import BaseModel, Field
+# from google import genai
+# from google.genai import types
+# from pydantic import BaseModel, Field
 
 from forms import RegistrationForm
 
@@ -15,8 +15,8 @@ load_dotenv()
 # env_path = base_dir / '.env'
 # load_dotenv(dotenv_path=env_path)
 FLASK_SECRET_KEY = os.getenv("FLASK_SECRET_KEY") 
-GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
-client = genai.Client(api_key=GEMINI_API_KEY)
+# GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
+# client = genai.Client(api_key=GEMINI_API_KEY)
 
 app = Flask(__name__)
 proxied = FlaskBehindProxy(app)
@@ -24,6 +24,10 @@ app.config['SECRET_KEY'] = FLASK_SECRET_KEY
 app.debug = True
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
+@app.context_processor
+def inject_proxy():
+    #change string to '' or /proxy/5000
+    return dict(proxy='')
 
 from auth import auth_bp
 app.register_blueprint(auth_bp)
